3d9f315d68c2d02cfa7d71902f4bd93392e4bfb0
JackyLu0520 <2758402509@qq.com>
2023-07-18 10:52:30 +0800

    poj2823.cpp[AC on luogu][TLE on poj]

--- poj2823.cpp
+++ poj2823.cpp
@@ -1,28 +1,30 @@
 #include<iostream>
-#include<queue>
+#include<deque>
 using namespace std;
 const int MAXN=1e6;
 int n,k;
-int a[MAXN+10],Max[MAXN+10],Min[MAXN+10];
-queue<int> q1,q2;
+int a[MAXN+10],Min[MAXN+10],Max[MAXN+10],minp=0,maxp=0;
+deque<int> q1,q2;
 int main(){
     cin>>n>>k;
     for(int i=1;i<=n;i++)
         cin>>a[i];
     for(int i=1;i<=n;i++){
-        while(!q1.empty()&&q1.size()>k) q1.pop();
-        while(!q2.empty()&&q2.size()>k) q2.pop();
-        while(!q1.empty()&&q1.back()<a[i]) q1.pop();
-        while(!q2.empty()&&q2.back()>a[i]) q2.pop();
-        q1.push(a[i]);
-        q2.push(a[i]);
-        Max[i]=q1.front();
-        Min[i]=q2.front();
+        while(!q1.empty()&&a[q1.back()]<=a[i]) q1.pop_back();
+        while(!q2.empty()&&a[q2.back()]>=a[i]) q2.pop_back();
+        q1.push_back(i);
+        q2.push_back(i);
+        while(!q1.empty()&&q1.front()<=i-k) q1.pop_front();
+        while(!q2.empty()&&q2.front()<=i-k) q2.pop_front();
+        if(i>=k){
+            Max[++maxp]=a[q1.front()];
+            Min[++minp]=a[q2.front()];
+        }
     }
-    for(int i=k;i<=n;i++)
+    for(int i=1;i<=minp;i++)
         cout<<Min[i]<<' ';
     cout<<endl;
-    for(int i=k;i<=n;i++)
+    for(int i=1;i<=maxp;i++)
         cout<<Max[i]<<' ';
     return 0;
 }
