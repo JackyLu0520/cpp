ffb4069519a6def101d3898f333550eeb3a51d10
JackyLu0520 <2758402509@qq.com>
2023-09-01 22:29:57 +0800

    P3369Slow[AC](?)

---
+++ P3369Slow.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+vector<int> a;
+int n;
+int main(){
+    scanf("%d",&n);
+    while(n--){
+        int opt,x;
+        scanf("%d%d",&opt,&x);
+        switch(opt){
+            case 1:a.insert(lower_bound(a.begin(),a.end(),x),x);break;
+            case 2:a.erase(lower_bound(a.begin(),a.end(),x));break;
+            case 3:printf("%d\n",(lower_bound(a.begin(),a.end(),x)-a.begin()+1));break;
+            case 4:printf("%d\n",a[x-1]);break;
+            case 5:printf("%d\n",*(lower_bound(a.begin(),a.end(),x)-1));break;
+            case 6:printf("%d\n",*upper_bound(a.begin(),a.end(),x));break;
+            default:break;
+        }
+        /*printf("\na:");
+        for(auto i:a)   printf("%d ",i);
+        printf("\n");*/
+    }
+    return 0;
+}
