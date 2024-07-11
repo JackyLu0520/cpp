80933e7272771a207c20b3c466ac68d9630f104f
JackyLu0520 <2758402509@qq.com>
2024-07-11 21:03:30 +0800

    P2085[unfinished]

---
+++ ssy/P2085.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef pair<int,int> pii;
+const int N=1e4+10;
+int n,m;
+int a[N],b[N],c[N];
+priority_queue<pii,vector<pii>,greater<pii>>q;
+int p[N];
+int val(int x){
+    return a[x]*p[x]*p[x]+b[x]*p[x]+c[x];
+}
+int main(){
+    scanf("%d%d",&n,&m);
+    for(int i=1;i<=n;i++)
+        scanf("%d%d%d",&a[i],&b[i],&c[i]);
+    for(int i=1;i<=n;i++)
+        q.push(make_pair(val(i),i));
+    for(int i=1;i<=m;i++){
+        pii t=q.top();q.pop();
+        printf("%d",t.first);
+        p[t.second]++;
+        q.push(make_pair(val(t.second),t.second));
+    }
+}
