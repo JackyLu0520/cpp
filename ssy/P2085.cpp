e00c1a037c54173d3bcf3e2c89578cecd8f25835
JackyLu0520 <2758402509@qq.com>
2024-07-11 21:05:34 +0800

    P2085[unfinished]

--- ssy/P2085.cpp
+++ ssy/P2085.cpp
@@ -13,12 +13,15 @@ int main(){
     scanf("%d%d",&n,&m);
     for(int i=1;i<=n;i++)
         scanf("%d%d%d",&a[i],&b[i],&c[i]);
-    for(int i=1;i<=n;i++)
+    for(int i=1;i<=n;i++){
+        p[i]=1;
         q.push(make_pair(val(i),i));
+    }
     for(int i=1;i<=m;i++){
         pii t=q.top();q.pop();
-        printf("%d",t.first);
+        printf("%d ",t.first);
         p[t.second]++;
         q.push(make_pair(val(t.second),t.second));
     }
+    return 0;
 }
