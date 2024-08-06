63dc0e24bf7ec26d8a67bb75172ead65e5f803ba
JackyLu0520 <2758402509@qq.com>
2024-08-06 15:03:57 +0800

    P1029[AC]

--- ssy/P1029.cpp
+++ ssy/P1029.cpp
@@ -7,11 +7,12 @@ long long gcd(int a,int b){
 long long ans=0;
 int main(){
     scanf("%lld%lld",&x,&y);
+    if(y%x!=0){printf("0");return 0;}
+    if(x==y){printf("1");return 0;}
     long long i;
     for(i=1;i*i<x*y;i++)
         if((x*y)%i==0&&gcd(i,x*y/i)==x)
             ans+=2;
-    if(i*i==x*y)  ans++;
     printf("%d",ans);
     return 0;
 }
---
+++ ssy/P1890.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+const int N=1e3+10,L=11;
+int n,m,a[N][L],lg[N];
+int gcd(int a,int b){
+    return b?gcd(b,a%b):a;
+}
+int main(){
+    scanf("%d%d",&n,&m);
+    for(int i=1;i<=n;i++)
+        scanf("%d",&a[i][0]);
+    for(int j=1;j<L;j++)
+        for(int i=1;i<=n;i++)
+            a[i][j]=gcd(a[i][j-1],a[i+(1<<(j-1))][j-1]);
+    lg[1]=0;
+    for(int i=2;i<=n;i++)
+        lg[i]=lg[i>>1]+1;
+    while(m--){
+        int l,r;
+        scanf("%d%d",&l,&r);
+        int t=lg[r-l+1];
+        printf("%d\n",gcd(a[l][t],a[r-(1<<t)+1][t]));
+    }
+    return 0;
+}
