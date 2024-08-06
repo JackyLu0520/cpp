e6c75a1c8260442df80f4437dfb1fc494d34d869
JackyLu0520 <2758402509@qq.com>
2024-08-06 14:44:32 +0800

    P1029[Waiting?]

--- ssy/P1029.cpp
+++ ssy/P1029.cpp
@@ -1,17 +1,17 @@
 #include<bits/stdc++.h>
 using namespace std;
-long long x0,y0;
+long long x,y;
 long long gcd(int a,int b){
     return b?gcd(b,a%b):a;
 }
 long long ans=0;
 int main(){
-    scanf("%lld%lld",&x0,&y0);
+    scanf("%lld%lld",&x,&y);
     long long i;
-    for(i=1;i*i<x0*y0;i++)
-        if((x0*y0)%i==0&&gcd(i,x0*y0/i)==x0)
+    for(i=1;i*i<x*y;i++)
+        if((x*y)%i==0&&gcd(i,x*y/i)==x)
             ans+=2;
-    if(i*i==x0*y0)  ans++;
+    if(i*i==x*y)  ans++;
     printf("%d",ans);
     return 0;
 }
