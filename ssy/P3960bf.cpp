1e01051d47209c90182252e693203ebc70915384
JackyLu0520 <2758402509@qq.com>
2024-08-13 08:05:02 +0800

    P3960bf[40](?)

--- ssy/P3960bf.cpp
+++ ssy/P3960bf.cpp
@@ -1,12 +1,12 @@
 #include<bits/stdc++.h>
 using namespace std;
-const int N=3e5+10,M=3e5+10;
+const int N=1e3+10,M=1e3+10;
 int n,m,q,a[N][M];
 int main(){
     scanf("%d%d%d",&n,&m,&q);
     for(int i=1;i<=n;i++)
         for(int j=1;j<=m;j++)
-            a[N][M]=(i-1)*m+j;
+            a[i][j]=(i-1)*m+j;
     while(q--){
         int x,y;
         scanf("%d%d",&x,&y);
