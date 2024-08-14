a8e8742f2d6fc46c19326fd2d61a0a6947e19501
JackyLu0520 <2758402509@qq.com>
2024-08-14 19:47:11 +0800

    P5490[AC]

---
+++ ssy/P5490.cpp
@@ -0,0 +1,77 @@
+#include<bits/stdc++.h>
+using namespace std;
+const int N=1e5+10;
+int n;
+int d[2*N],dtot;
+struct node{
+    int l,r;
+    long long tag,len;
+}t[16*N];
+void pushup(int p){
+    if(t[p].tag)
+        t[p].len=d[t[p].r+1]-d[t[p].l];
+    else
+        t[p].len=t[2*p].len+t[2*p+1].len;
+}
+void build(int p,int l,int r){
+    t[p].l=l;t[p].r=r;
+    if(l==r){t[p].tag=0;t[p].len=0;return;}
+    int mid=(l+r)/2;
+    build(2*p,l,mid);build(2*p+1,mid+1,r);
+    pushup(p);
+}
+void add(int p,int l,int r,long long k){
+    if(l<=t[p].l&&r>=t[p].r){
+        t[p].tag+=k;
+        pushup(p);
+        return;
+    }
+    int mid=(t[p].l+t[p].r)/2;
+    if(l<=mid)  add(2*p,l,r,k);
+    if(r>mid)   add(2*p+1,l,r,k);
+    pushup(p);
+}
+/*void preorder(int p){
+    printf("[%d,%d]:tag=%lld,len=%lld\n",t[p].l,t[p].r,t[p].tag,t[p].len);
+    if(t[p].l!=t[p].r){preorder(2*p);preorder(2*p+1);}
+}*/
+struct op{
+    int add,x,l,r;
+    op(){}
+    op(int A,int X,int L,int R){add=A;x=X;l=L;r=R;}
+    bool operator < (const op& a) const{
+        return x<a.x;
+    }
+}o[2*N];
+long long ans=0;
+int main(){
+    scanf("%d",&n);
+    for(int i=1;i<=n;i++){
+        int x1,y1,x2,y2;
+        scanf("%d%d%d%d",&x1,&y1,&x2,&y2);
+        d[2*i-1]=x1;d[2*i]=x2;
+        o[2*i-1]=op(1,min(y1,y2),min(x1,x2),max(x1,x2));
+        o[2*i]=op(-1,max(y1,y2),min(x1,x2),max(x1,x2));
+    }
+    sort(d+1,d+2*n+1);
+    dtot=unique(d+1,d+2*n+1)-d-1;
+    build(1,1,dtot-1);
+    sort(o+1,o+2*n+1);
+    /*for(int i=1;i<=dtot;i++)
+        printf("%d ",d[i]);
+    printf("\n");
+    for(int i=1;i<=2*n;i++)
+        printf("[%d,%d] %d %d\n",o[i].l,o[i].r,o[i].x,o[i].add);
+    printf("\n");
+    preorder(1);*/
+    for(int i=1;i<2*n;i++){
+        int l=lower_bound(d+1,d+dtot+1,o[i].l)-d;
+        int r=lower_bound(d+1,d+dtot+1,o[i].r)-d-1;
+        add(1,l,r,o[i].add);
+        ans+=t[1].len*(o[i+1].x-o[i].x);
+        //printf("----------[%d,%d]----------\n",l,r);
+        //preorder(1);
+    }
+    printf("%lld",ans);
+    return 0;
+}
