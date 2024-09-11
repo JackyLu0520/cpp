f6c504c0bde36c890e82282d12dd9de1bf6d3b0b
JackyLu0520 <2758402509@qq.com>
2024-09-11 21:13:33 +0800

    P8436[AC]

--- P8436.cpp
+++ P8436.cpp
@@ -1,31 +1,56 @@
 #include<bits/stdc++.h>
 using namespace std;
-const int MAXN=5e4;
-int n,m,t,ans,dfn[MAXN+10],low[MAXN+10];
-vector<int> adj[MAXN+10];
-void tarjan(int u,int pre){
-	dfn[u]=low[u]=++t;
-	for(int v:adj[u]){
-		if(v==pre)	continue;
+const int N=5e5+10,M=2e6+10;
+int n,m;
+int head[N],nxt[2*M],ver[2*M],tot=1;
+void add(int u,int v){
+	ver[++tot]=v;nxt[tot]=head[u];head[u]=tot;
+}
+int dfn[N],low[N],bridge[2*M],dfsid;
+void tarjan(int u,int in_edge){
+	dfn[u]=low[u]=++dfsid;
+	for(int i=head[u];i;i=nxt[i]){
+		int v=ver[i];
 		if(!dfn[v]){
-			tarjan(v,u);
+			tarjan(v,i);
 			low[u]=min(low[u],low[v]);
-			if(dfn[u]<low[v])
-				ans++;
-		}else   low[u]=min(low[u],dfn[v]);
+			if(low[v]>dfn[u])
+				bridge[i]=bridge[i^1]=1;
+		}else if(i!=(in_edge^1))
+			low[u]=min(low[u],dfn[v]);
+	}
+}
+int c[N],dccid;
+vector<int>cc[N];
+void dfs(int u){
+	c[u]=dccid;cc[dccid].push_back(u);
+	for(int i=head[u];i;i=nxt[i]){
+		int v=ver[i];
+		if(c[v]||bridge[i])	continue;
+		dfs(v);
 	}
 }
 int main(){
-	cin>>n>>m;
+	scanf("%d%d",&n,&m);
 	for(int i=1;i<=m;i++){
 		int u,v;
-		cin>>u>>v;
-		adj[u].push_back(v);
-		adj[v].push_back(u);
+		scanf("%d%d",&u,&v);
+		add(u,v);add(v,u);
 	}
-	for(int u=1;u<=n;u++){
-		if(!dfn[u])	tarjan(u,u);
+	for(int i=1;i<=n;i++)
+		if(!dfn[i])
+			tarjan(i,0);
+	for(int i=1;i<=n;i++)
+		if(!c[i]){
+			dccid++;
+			dfs(i);
+		}
+	printf("%d\n",dccid);
+	for(int i=1;i<=dccid;i++){
+		printf("%d ",cc[i].size());
+		for(int &j:cc[i])
+			printf("%d ",j);
+		printf("\n");
 	}
-	cout<<ans<<endl;
 	return 0;
 }
