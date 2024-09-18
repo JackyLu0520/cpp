2d61e75bac75ee39d15f822e9d86fe359a21b9e5
JackyLu0520 <2758402509@qq.com>
2024-09-18 19:25:52 +0800

    P1613[AC]

--- ssy/P1613.cpp
+++ ssy/P1613.cpp
@@ -1,13 +1,27 @@
 #include<bits/stdc++.h>
 using namespace std;
-const int N=65,L=32;
+const int N=65,L=65;
 int n,m;
 int a[N][N],dp[N][N][L];
 int main(){
+    memset(a,0x3f,sizeof a);
     scanf("%d%d",&n,&m);
-    for(int i=1;i<=n;i++){
+    for(int i=1;i<=m;i++){
         int u,v;scanf("%d%d",&u,&v);
-        dp[u][v][0]=1;
+        a[u][v]=1;dp[u][v][0]=1;
     }
-    for(int t=1;)
+    for(int t=1;t<L;t++)
+        for(int k=1;k<=n;k++)
+            for(int i=1;i<=n;i++)
+                for(int j=1;j<=n;j++)
+                    if(dp[i][k][t-1]&&dp[k][j][t-1]){
+                        dp[i][j][t]=1;
+                        a[i][j]=1;
+                    }
+    for(int k=1;k<=n;k++)
+        for(int i=1;i<=n;i++)
+            for(int j=1;j<=n;j++)
+                a[i][j]=min(a[i][j],a[i][k]+a[k][j]);
+    printf("%d",a[1][n]);
+    return 0;
 }
