6803c89314d881a0c46acbea61b3c93261b29dbf
JackyLu0520 <2758402509@qq.com>
2024-09-18 19:14:25 +0800

    B3611bitset[AC,33ms]

--- ssy/B3611bitset.cpp
+++ ssy/B3611bitset.cpp
@@ -1,14 +1,18 @@
-#include<bits/stdc++.h>
+#include<cstdio>
+#include<bitset>
 using namespace std;
 const int N=110;
 int n;
 bitset<N> a[N];
 int main(){
-    scanf("%d",&n);
+    //scanf("%d",&n);
+    char c;
+    while(1){c=getchar();if(c<'0'||c>'9')break;n=(n<<3)+(n<<1)+(c^48);}
     for(int i=1;i<=n;i++)
         for(int j=1;j<=n;j++){
-            int t;scanf("%d",&t);
-            a[i].set(j,t);
+            char t;
+            do{t=getchar();}while(t<'0'||t>'1');
+            a[i].set(j,t-'0');
         }
     for(int j=1;j<=n;j++)
         for(int i=1;i<=n;i++)
