d23923e9783ff89e46a4faa50c5a7145617aaa92
JackyLu0520 <2758402509@qq.com>
2024-11-13 10:16:30 +0800

    P7116[30]

--- ssy/P7116.cpp
+++ ssy/P7116.cpp
@@ -5,23 +5,43 @@ int n,k;
 int w[K],c[N],d[N];
 int now[K];
 int ans=0;
+int cnt[10000010];
+int num(int t[]){
+    int ret=0;
+    for(int i=k;i>=1;i--)
+        ret=ret*w[i]+t[i];
+    return ret;
+}
 bool dfs(int step){
     if(step==k+1){
         int t[K];
         for(int i=1;i<=k;i++)
             t[i]=now[i];
+        if(cnt[num(t)]){
+            ans=(ans+cnt[num(t)])%MOD;
+            return 1;
+        }
+        int cnt1=0;
         while(1){
             bool f=0;
             for(int i=1;i<=n;i++){
                 t[c[i]]+=d[i];
-                if(t[c[i]]<1||t[c[i]]>w[c[i]])
+                cnt1=(cnt1+1)%MOD;
+                if(t[c[i]]<1||t[c[i]]>w[c[i]]){
+                    ans=(ans+cnt1)%MOD;
+                    cnt[num(now)]=cnt1;
                     return 1;
-                ans=(ans+1)%MOD;
+                }
             }
             for(int i=1;i<=k;i++)
                 if(t[i]!=now[i])
                     f=1;
             if(!f)return 0;
+            if(cnt[num(t)]){
+                ans=(ans+cnt[num(t)]+cnt1)%MOD;
+                cnt[num(now)]=cnt[num(t)]+cnt1;
+                return 1;
+            }
         }
         return 1;
     }
