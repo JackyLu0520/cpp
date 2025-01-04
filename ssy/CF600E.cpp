65de78f8d7a9bfa5e0f67efd0887ce48cc16afd4
JackyLu0520 <2758402509@qq.com>
2025-01-04 21:12:42 +0800

    CF600E[?]

--- ssy/CF600E.cpp
+++ ssy/CF600E.cpp
@@ -6,22 +6,29 @@ int head[N],nxt[2*N],ver[2*N],tot=1;
 void add(int u,int v){
     ver[++tot]=v;nxt[tot]=head[u];head[u]=tot;
 }
-int size[N],son[N];
-void dfs1(int u,int p){
-    size[u]=1;son[u]=0;
-    int maxsize=0;
+int ans[N],mx[N];
+map<int,int>cnt[N];
+void dfs(int u,int p){
+    cnt[u][c[u]]=1;
+    ans[u]=c[u];
+    mx[u]=1;
     for(int i=head[u];i;i=nxt[i]){
         int v=ver[i];
         if(v==p)    continue;
-        dfs1(v,u);
-        size[u]+=size[v];
-        if(size[v]>maxsize)
-            maxsize=size[v],son[u]=v;
+        dfs(v,u);
+        if(cnt[u].size()<cnt[v].size()){
+            cnt[u].swap(cnt[v]);
+            ans[u]=ans[v];
+            mx[u]=mx[v];
+        }
+        for(auto &i:cnt[v]){
+            cnt[u][i.first]+=i.second;
+            if(cnt[u][i.first]>mx[u])
+                mx[u]=cnt[u][i.first],ans[u]=0;
+            if(cnt[u][i.first]==mx[u])
+                ans[u]+=i.first;
+        }
     }
-}
-int ans[N];
-void dfs2(int u,int p){
-
 }
 int main(){
     scanf("%d",&n);
@@ -33,6 +40,9 @@ int main(){
         add(u,v);
         add(v,u);
     }
-    dfs1(1,0);
-
+    dfs(1,0);
+    for(int i=1;i<=n;i++)
+        printf("%d ",ans[i]);
+    printf("\n");
+    return 0;
 }
