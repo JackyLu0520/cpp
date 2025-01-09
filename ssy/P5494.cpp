b8588c4b82ef1eaa7ee159b804212df268f297bd
JackyLu0520 <2758402509@qq.com>
2025-01-09 16:49:18 +0800

    P5494[AC]

--- ssy/P5494.cpp
+++ ssy/P5494.cpp
@@ -1,4 +1,5 @@
 #include<bits/stdc++.h>
+#define int long long
 using namespace std;
 const int N=2e5+10,M=2e5+10;
 int n,m;
@@ -9,18 +10,20 @@ struct node{
     int size;
 };
 node *root[M];int tot=1;
+node *null;
 inline node *newnode(int val,int cnt){
     node *p=new node;
-    p->ls=p->rs=nullptr;
+    p->ls=p->rs=null;
     p->val=val;p->cnt=cnt;
     p->pri=rand();
     p->size=cnt;
+    return p;
 }
 inline void pushup(node *p){
-    p->size=p->ls->size+p->rs->size+1;
+    p->size=p->ls->size+p->rs->size+p->cnt;
 }
 void split(node *p,int val,node *&l,node *&r){
-    if(p==nullptr){l=r=nullptr;return;}
+    if(p==null){l=r=null;return;}
     if(p->val>val){
         r=p;
         split(p->ls,val,l,p->ls);
@@ -31,8 +34,8 @@ void split(node *p,int val,node *&l,node *&r){
     pushup(p);
 }
 node *merge(node *l,node *r){
-    if(l==nullptr)  return r;
-    if(r==nullptr)  return l;
+    if(l==null)  return r;
+    if(r==null)  return l;
     if(l->pri>r->pri){
         l->rs=merge(l->rs,r);
         pushup(l);
@@ -44,17 +47,17 @@ node *merge(node *l,node *r){
     }
 }
 node *Merge(node *p,node *q){
-    if(p==nullptr)  return q;
-    if(q==nullptr)  return p;
+    if(p==null)  return q;
+    if(q==null)  return p;
     if(p->pri<q->pri)
         swap(p,q);
     node *l,*mid,*r;
     split(q,p->val,l,r);
     split(l,p->val-1,l,mid);
-    if(mid!=nullptr){
+    if(mid!=null){
         p->cnt+=mid->cnt;
         delete mid;
-        mid=nullptr;
+        mid=null;
     }
     p->ls=Merge(p->ls,l);
     p->rs=Merge(p->rs,r);
@@ -66,8 +69,8 @@ inline void add(node *&p,int val,int cnt){
     node *l,*mid,*r;
     split(p,val,l,r);
     split(l,val-1,l,mid);
-    if(mid==nullptr)    mid=newnode(val,cnt);
-    else                mid->cnt+=cnt;
+    if(mid==null)   mid=newnode(val,cnt);
+    else            mid->cnt+=cnt;
     pushup(mid);
     p=merge(merge(l,mid),r);
 }
@@ -85,20 +88,35 @@ int queryval(node *p,int rank){
     else if(p->size>=rank)  return queryval(p->rs,rank-p->ls->size-p->cnt);
     else    return -1;
 }
-int main(){
-    scanf("%d%d",&n,&m);
+#ifdef DEBUG
+void inorder(node *p){
+    if(p==null) return;
+    inorder(p->ls);
+    printf("%lld:%lld ",p->val,p->cnt);
+    inorder(p->rs);
+}
+#endif
+signed main(){
+
+    null=new node;
+    null->ls=null->rs=null;
+    null->val=null->cnt=0;
+    null->pri=-1;null->size=0;
+
+    scanf("%lld%lld",&n,&m);
+    root[1]=null;
     for(int i=1;i<=n;i++){
         int cnt;
-        scanf("%d",&cnt);
+        scanf("%lld",&cnt);
         add(root[1],i,cnt);
     }
     while(m--){
         int op;
-        scanf("%d",&op);
+        scanf("%lld",&op);
         int p,x,y,k;
         switch(op){
             case 0:
-                scanf("%d%d%d",&p,&x,&y);
+                scanf("%lld%lld%lld",&p,&x,&y);
                 node *l,*mid,*r;
                 split(root[p],y,l,r);
                 split(l,x-1,l,mid);
@@ -106,20 +124,30 @@ int main(){
                 root[++tot]=mid;
             break;
             case 1:
-                scanf("%d%d",&p,&k);
+                scanf("%lld%lld",&p,&k);
                 root[p]=Merge(root[p],root[k]);
             break;
             case 2:
-                scanf("%d%d%d",&p,&k,&x);
+                scanf("%lld%lld%lld",&p,&k,&x);
                 add(root[p],x,k);
             break;
             case 3:
-                scanf("%d%d%d",&p,&x,&y);
-                printf("%d\n",querysize(root[p],x,y));
+                scanf("%lld%lld%lld",&p,&x,&y);
+                printf("%lld\n",querysize(root[p],x,y));
             break;
             case 4:
-                scanf("%d%d",&p,&k);
-                printf("%d\n",queryval(root[p],k));
+                scanf("%lld%lld",&p,&k);
+                printf("%lld\n",queryval(root[p],k));
+            break;
+#ifdef DEBUG
+            case 5:
+                scanf("%lld",&p);
+                printf("%lld|",p);
+                inorder(root[p]);
+                printf("\n");
+            break;
+#endif
+            default:
             break;
         }
     }
