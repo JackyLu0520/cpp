8faba04e73e7ecc72e3d60fb93c352a8f5400a22
JackyLu0520 <2758402509@qq.com>
2025-01-13 21:11:06 +0800

    clear

--- clear.ps1
+++ clear.ps1
@@ -1,3 +1,4 @@
 Get-ChildItem -Recurse -File | Where { -not $_.extension } | Remove-Item
 Remove-Item -Recurse * -Include *.exe
 Remove-Item -Recurse * -Include *._*
+Remove-Item -Recurse * -Include *.o
