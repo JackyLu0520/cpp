066101899c87895bbf2bcfba535d6279fdbdb9ec
JackyLu0520 <2758402509@qq.com>
2025-01-13 10:01:04 +0800

    P3146a[AC]

---
+++ ssy/P3146a.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+const int N=260;
+int n,dp[N][N],ans=0;
+int main(){
+    scanf("%d",&n);
+    for(int i=1;i<=n;i++){
+        scanf("%d",&dp[i][i]);
+        ans=max(ans,dp[i][i]);
+    }
+    for(int j=1;j<=n;j++)
+        for(int i=j-1;i>=1;i--){
+            for(int k=i;k<j;k++)
+                if(dp[i][k]==dp[k+1][j]&&dp[i][k])
+                    dp[i][j]=max(dp[i][j],dp[i][k]+1);
+            ans=max(ans,dp[i][j]);
+        }
+    printf("%d",ans);
+    return 0;
+}
